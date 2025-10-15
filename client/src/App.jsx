154a77cfb84aa1bf7439fd4cb274cbdd336c845e
Osman Bin Nasir <osmanbinnasir19@gmail.com>
2025-10-15 17:41:08 +0530

    feat: Refactor client-side structure and add new features

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,20 @@
 import { useState } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import MenuManagement from './pages/MenuManagement'
+import MenuManagement from './pages/Admin/MenuManagement'
 
 
 function App() {
-  return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/menu" element={<MenuManagement />} />
-        </Routes>
-      </Router>
+    return (
+        <>
+            <Router>
+                <Routes>
+                    <Route path="/menu" element={<MenuManagement />} />
+                </Routes>
+            </Router>
 
-    </>
-  )
+        </>
+    )
 }
 
 export default App
